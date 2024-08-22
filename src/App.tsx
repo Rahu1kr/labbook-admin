3adfb06494c349df24348f2bbdbbf353638fd1d8
Rahu1kr <88312053+Rahu1kr@users.noreply.github.com>
2024-08-22 19:12:11 +0530

    Add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
     <div>
-      
+      <h1>Welcome TO ShadCN</h1>
+      <Button>Click Me</Button>
     </div>
     </>
   )
